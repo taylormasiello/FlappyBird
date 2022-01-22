c31611febdd698cc8807ddd8ab29d955c864ab1b
Taylor Masiello <masiellotay@gmail.com>
2022-01-22 09:17:19 -0800

    Added gameTimer, pipeSpeed and movement across screen, added gravity and spacebar behavior to flappyBird

--- FlappyBirdWinForms/Form1.Designer.cs
+++ FlappyBirdWinForms/Form1.Designer.cs
@@ -28,11 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pipeTop = new System.Windows.Forms.PictureBox();
             this.pipeBottom = new System.Windows.Forms.PictureBox();
             this.flappyBird = new System.Windows.Forms.PictureBox();
             this.ground = new System.Windows.Forms.PictureBox();
             this.scoreText = new System.Windows.Forms.Label();
+            this.gmTimer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).BeginInit();
@@ -42,7 +44,7 @@
             // pipeTop
             // 
             this.pipeTop.Image = global::FlappyBirdWinForms.Properties.Resources.pipedown;
-            this.pipeTop.Location = new System.Drawing.Point(667, -58);
+            this.pipeTop.Location = new System.Drawing.Point(493, -66);
             this.pipeTop.Name = "pipeTop";
             this.pipeTop.Size = new System.Drawing.Size(100, 323);
             this.pipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -52,7 +54,7 @@
             // pipeBottom
             // 
             this.pipeBottom.Image = global::FlappyBirdWinForms.Properties.Resources.pipe;
-            this.pipeBottom.Location = new System.Drawing.Point(491, 439);
+            this.pipeBottom.Location = new System.Drawing.Point(366, 449);
             this.pipeBottom.Name = "pipeBottom";
             this.pipeBottom.Size = new System.Drawing.Size(100, 320);
             this.pipeBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -62,12 +64,13 @@
             // flappyBird
             // 
             this.flappyBird.Image = global::FlappyBirdWinForms.Properties.Resources.bird;
-            this.flappyBird.Location = new System.Drawing.Point(43, 238);
+            this.flappyBird.Location = new System.Drawing.Point(43, 233);
             this.flappyBird.Name = "flappyBird";
             this.flappyBird.Size = new System.Drawing.Size(90, 69);
             this.flappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.flappyBird.TabIndex = 2;
             this.flappyBird.TabStop = false;
+            this.flappyBird.Click += new System.EventHandler(this.flappyBird_Click);
             // 
             // ground
             // 
@@ -83,18 +86,24 @@
             // 
             this.scoreText.AutoSize = true;
             this.scoreText.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.scoreText.Location = new System.Drawing.Point(12, 12);
+            this.scoreText.Location = new System.Drawing.Point(2, 9);
             this.scoreText.Name = "scoreText";
             this.scoreText.Size = new System.Drawing.Size(232, 56);
             this.scoreText.TabIndex = 4;
             this.scoreText.Text = "Score: 0";
+            // 
+            // gmTimer
+            // 
+            this.gmTimer.Enabled = true;
+            this.gmTimer.Interval = 20;
+            this.gmTimer.Tick += new System.EventHandler(this.gmTimerEvent);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.ClientSize = new System.Drawing.Size(883, 820);
+            this.ClientSize = new System.Drawing.Size(725, 820);
             this.Controls.Add(this.scoreText);
             this.Controls.Add(this.ground);
             this.Controls.Add(this.flappyBird);
@@ -102,6 +111,8 @@
             this.Controls.Add(this.pipeTop);
             this.Name = "Form1";
             this.Text = "Flappy Bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gmKeyDwn);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gmKeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).EndInit();
@@ -118,6 +129,7 @@
         private System.Windows.Forms.PictureBox flappyBird;
         private System.Windows.Forms.PictureBox ground;
         private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer gmTimer;
     }
 }
 
--- FlappyBirdWinForms/Form1.cs
+++ FlappyBirdWinForms/Form1.cs
@@ -12,9 +12,63 @@ namespace FlappyBirdWinForms
 {
     public partial class Form1 : Form
     {
+
+        int pipeSpeed = 10;
+        int gravity = 15;
+        int score = 0;
+        
         public Form1()
         {
             InitializeComponent();
         }
+
+
+
+        private void gmKeyDwn(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                gravity = -13;
+            }
+        }
+
+        private void gmKeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                gravity = 15;
+            }
+        }
+
+        private void endGame()
+        {
+            gmTimer.Stop();
+            scoreText.Text += " Game Over...Try Again?";
+        }
+
+        private void gmTimerEvent(object sender, EventArgs e)
+        {
+            //dwn"pushes the flappyBird dwn, by adding gravity to the top its top"
+            flappyBird.Top += gravity;
+            pipeBottom.Left -= pipeSpeed;
+            pipeTop.Left -= pipeSpeed;
+            //^"pulls pipes to left of the screen from right (left being 0, reduces pipeSpeed from left position with each tick of timer)"
+            scoreText.Text = "Score: " + score;
+
+            //dwn"checks if pipes have left screen; if pipe location is too low, will reset it to our given value"
+            if (pipeBottom.Left < -100)
+            {
+                pipeBottom.Left = 800;
+            }
+            if (pipeTop.Left < -100)
+            {
+                pipeTop.Left = 950;
+            }
+        }
+
+        //private void flappyBird_Click(object sender, EventArgs e)
+        //{
+
+        //}
     }
 }
